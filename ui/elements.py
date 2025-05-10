c8ab080b91188ef647c84d16343a23483bc1f576
casper <johnwaiton@hotmail.co.uk>
2025-05-10 22:00:13 +0100

    add acquisition button

--- ui/elements.py
+++ ui/elements.py
@@ -103,3 +103,44 @@ class ConnectDigitiser(QGroupBox):
         logging.info(f'Attempting connection to {self.controller}...')
         # if not connected, try to connect.
         self.controller.connect_digitiser()
+
+
+class Acquisition(QGroupBox):
+    '''
+    Acquisition control panel for the digitiser.
+    Start and stop button to start and stop the acquisition.
+    '''
+    def __init__(self, controller, parent=None):
+        super().__init__("Acquisition", parent = parent)
+        
+        self.controller = controller
+        
+
+        self.start_stop = QPushButton("Start")
+        self.start_stop.setStyleSheet("background-color: green; color: black")
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(self.start_stop)
+
+        self.isAcquiring = False
+        self.start_stop.clicked.connect(self.toggle_acquisition)
+    
+
+    def toggle_acquisition(self):
+        if self.isAcquiring:
+            logging.info('Stopping acquisition...')
+            self.start_stop.setText("Start")
+            self.start_stop.setStyleSheet("background-color: green; color: black")
+            self.isAcquiring = False
+            # stop the acquisition
+            # self.controller.stop_acquisition()
+        else:
+            logging.info('Starting acquisition...')
+            self.start_stop.setText("Stop")
+            self.start_stop.setStyleSheet("background-color: red; color: white")
+            self.isAcquiring = True
+            # start the acquisition
+            # self.controller.start_acquisition()
+    
--- ui/oscilloscope.py
+++ ui/oscilloscope.py
@@ -36,14 +36,16 @@ class ControlPanel(QFrame):
         self.setFrameShape(QFrame.StyledPanel)
 
         self.connect_digitiser = elements.ConnectDigitiser(self.controller)
-        self.stats_box = elements.StatsBox()
-        self.conf_files = elements.config_files()
+        self.stats_box         = elements.StatsBox()
+        self.conf_files        = elements.config_files()
+        self.acquisition       = elements.Acquisition(self.controller)
 
         self.layout = QVBoxLayout()
 
         self.layout.addWidget(self.connect_digitiser)
         self.layout.addWidget(self.stats_box)
         self.layout.addWidget(self.conf_files)
+        self.layout.addWidget(self.acquisition)
 
         self.layout.addStretch()
 
