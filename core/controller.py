8fadd6b168f56388f77fa92f06c4b6694c1c24cb
mmillns2 <matthewmillns@outlook.com>
2025-11-09 22:49:36 +0000

    Made Tracker thread-safe

--- core/controller.py
+++ core/controller.py
@@ -334,18 +334,20 @@ class Tracker:
         self.bytes_ps   = 0
         self.events_ps  = 0
         self.last_time  = self.start_time
+        self.lock       = Lock()
 
     def track(self, nbytes: int = 0):
         '''
         Tracker outputting the number of events that arrive per second
         '''
-        self.events_ps += 1
-        self.bytes_ps += nbytes
+        with self.lock:
+            self.events_ps += 1
+            self.bytes_ps += nbytes
 
-        t_check = time.perf_counter()
-        if t_check - self.last_time >= 1.0:
-            MB = self.bytes_ps / 1000000
-            logging.info(f'|| {self.events_ps} events/sec || {MB:.2f} MB/sec ||')
-            self.last_time = t_check
-            self.bytes_ps = 0
-            self.events_ps = 0
+            t_check = time.perf_counter()
+            if t_check - self.last_time >= 1.0:
+                MB = self.bytes_ps / 1000000
+                logging.info(f'|| {self.events_ps} events/sec || {MB:.2f} MB/sec ||')
+                self.last_time = t_check
+                self.bytes_ps = 0
+                self.events_ps = 0
